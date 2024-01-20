2df6da04c481e24dae5daf34f60f778e706808a6
Michael <trololololort@mail.ru>
2024-01-20 07:07:55 +0300

    first commit

---
+++ products/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Country(models.Model):
+    name = models.CharField(max_length=100,
+                            null=False,
+                            blank=False,
+                            default="",
+                            verbose_name="Наименование")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Страна"
+        verbose_name_plural = "Страны"
+
